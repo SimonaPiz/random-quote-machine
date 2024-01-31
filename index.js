774f685c59a563eb5f192f56331fa997a4878e1c
Simona Pizio <simona.pizio@gmail.com>
2024-01-31 10:08:44 +0100

    features #1 - Fix initial random loading

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ let quote ='Happiness is the real sense of fulfillment that comes from hard work
 let author = '- Joseph Barbara';
 
 const getQuote = () => {
-  $.ajax({
-      method: 'GET',
-      url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
-      headers: { 'X-Api-Key': apiKey},
-      contentType: 'application/json',
-      success: function(result) {
-          //console.log(result[0],result[0].author);
-          quote = result[0].quote;
-          author = '- ' + result[0].author;
-          console.log(quote,author);
-      },
-      error: function ajaxError(jqXHR) {
-          console.error('Error: ', jqXHR.responseText);
-      }
+  return $.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/quotes?category=' + category,
+    headers: { 'X-Api-Key': apiKey},
+    contentType: 'application/json',
+    success: function(result) {
+      //console.log(result[0],result[0].author);
+      quote = result[0].quote;
+      author = '- ' + result[0].author;
+      console.log(quote,author);
+    },
+    error: function ajaxError(jqXHR) {
+      console.error('Error: ', jqXHR.responseText);
+    }
   });
 };
 
@@ -29,11 +29,13 @@ const setNewQuote = (newQuote, newAuthor) => {
 }
 
 $(document).ready(function(){
-  setNewQuote(quote,author);
-  getQuote();
+  getQuote().then(()=> {
+    setNewQuote(quote,author);
+  });
 
   $( "#new-quote" ).on( "click", function() {
-    getQuote();
-    setNewQuote(quote,author);
+    getQuote().then(()=> {
+      setNewQuote(quote,author);
+    });
   } );
 });
