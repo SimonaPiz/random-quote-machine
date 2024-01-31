225c376741f9c866867883604c6f402b856d1f37
Simona Pizio <simona.pizio@gmail.com>
2024-01-31 11:23:00 +0100

    features #3 - Change colors on click

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const apiKey = 'cuAKWWU0SfGHz/JjaQn/wg==1xZziCcQHnDAFXGW';
 
 let quote ='Happiness is the real sense of fulfillment that comes from hard work.';
 let author = '- Joseph Barbara';
+const colors = [
+  "#00008B",
+  "#191970",
+  "#800080",
+  "#A52A2A",
+  "#DC143C",
+  "#006400",
+  "#556B2F",
+  "#8B008B",
+  "#696969",
+  "#2E8B57"
+];
 
 const getQuote = () => {
   return $.ajax({
@@ -15,7 +27,7 @@ const getQuote = () => {
       //console.log(result[0],result[0].author);
       quote = result[0].quote;
       author = '- ' + result[0].author;
-      console.log(quote,author);
+      //console.log(quote,author);
     },
     error: function ajaxError(jqXHR) {
       console.error('Error: ', jqXHR.responseText);
@@ -24,9 +36,11 @@ const getQuote = () => {
 };
 
 const setNewQuote = (newQuote, newAuthor) => {
+  // change quote e author
   $("#text").text(newQuote);
   $("#author").text(newAuthor);
 
+  // change links
   $('#tweet-quote').attr(
     'href',
     'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
@@ -41,6 +55,12 @@ const setNewQuote = (newQuote, newAuthor) => {
       encodeURIComponent(newQuote) +
       '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
   );
+
+  //change colors
+  const colorIdx = Math.floor(Math.random() * colors.length);
+  console.log(colorIdx, colors[colorIdx])
+  $('.bg-color').css("backgroundColor", colors[colorIdx]);
+  $('.color').css("color", colors[colorIdx]);
 }
 
 $(document).ready(function(){
